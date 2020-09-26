04958514dfeb0a00c1401a4bfcd87071fdb42ccc
kazimirm <kazimir.majo@gmail.com>
2020-09-26 22:07:46 +0200

    graph code refactoring and improvements

--- app/src/main/java/com/kazimir/pedometer/MainActivity.java
+++ app/src/main/java/com/kazimir/pedometer/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.LimitLine;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
@@ -50,18 +51,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         ArrayList<BarEntry> barEntries = new ArrayList<>();
         insertDummyValuesToDb();
         ArrayList<Integer> colors = new ArrayList<>();
+
         for (int i = 0; i <= 6; i++){
             int steps = db.getStepCountForDay(getDateForDayMinusN(i + 1, "yyyyMMdd"));
             BarEntry barEntry = new BarEntry( i, steps);
             barEntries.add(barEntry);
             colors.add(steps >= DAY_MIN ? Color.GREEN : Color.RED);
         }
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Steps");
-        barDataSet.setValueTextColor(getResources().getColor(android.R.color.white));
-        barDataSet.setColors(colors);
-
-
-
 
         ArrayList<String> days = new ArrayList<>();
         days.add(getDateForDayMinusN(7,"dd.MM"));
@@ -72,10 +68,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         days.add(getDateForDayMinusN(2,"dd.MM"));
         days.add(getDateForDayMinusN(1,"dd.MM"));
 
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Steps");
+        barDataSet.setValueTextColor(Color.WHITE);
+        barDataSet.setValueTextSize(10);
+        barDataSet.setColors(colors);
+
         XAxis xAxis = barChart.getXAxis();
         xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
 
+        LimitLine ll = new LimitLine(DAY_MIN);
+        ll.setLineWidth(4f);
+        ll.setLineColor(Color.GREEN);
+        barChart.getAxisLeft().addLimitLine(ll);
         barChart.getDescription().setEnabled(false);
         barChart.setDrawGridBackground(false);
 
@@ -87,7 +92,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         barChart.setDrawBorders(false);
         barChart.setDrawGridBackground(false);
 
-
         barChart.getDescription().setEnabled(false);
         barChart.getLegend().setEnabled(false);
 
@@ -97,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         barChart.getXAxis().setDrawGridLines(false);
         barChart.getXAxis().setDrawAxisLine(false);
-        barChart.getXAxis().setTextColor(getResources().getColor(android.R.color.white));
+        barChart.getXAxis().setTextColor(Color.WHITE);
 
         barChart.getAxisRight().setDrawGridLines(false);
         barChart.getAxisRight().setDrawLabels(false);
