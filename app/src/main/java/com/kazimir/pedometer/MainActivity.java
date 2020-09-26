7089caf5b34754fed58b073f4c4af3d271ca7cda
kazimirm <kazimir.majo@gmail.com>
2020-09-26 16:13:23 +0200

    basic functionality to count steps and display count

--- app/src/main/java/com/kazimir/pedometer/MainActivity.java
+++ app/src/main/java/com/kazimir/pedometer/MainActivity.java
@@ -1,13 +1,64 @@
 package com.kazimir.pedometer;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.*;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManager;
+    TextView tv_steps;
+    boolean running = false;
+    private SensorEvent event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_steps = (TextView) findViewById(R.id.tv_steps);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null){
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Sensor not found!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+        //this will stop detecting steps
+        //sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (running){
+            int dailySteps = (int) event.values[0];
+            tv_steps.setText(String.valueOf(dailySteps));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
