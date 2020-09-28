fd96363ea4734d7cc447342fa191d681eb3510a7
kazimirm <kazimir.majo@gmail.com>
2020-09-28 16:18:20 +0200

    bugfix

--- app/src/main/java/com/kazimir/pedometer/MainActivity.java
+++ app/src/main/java/com/kazimir/pedometer/MainActivity.java
@@ -185,7 +185,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             todayOffset = since_boot;
             todaysDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
             todaySteps++;
-            db.updateDayDataOrCreate(new DayData(todaysDate, todaySteps));
+            database.updateDayDataOrCreate(new DayData(todaysDate, todaySteps));
         } else {
             todaySteps = since_boot - todayOffset;
             textViewSteps.setText(String.valueOf(todaySteps));
